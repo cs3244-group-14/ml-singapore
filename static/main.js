caf08381aa5fdc7b6f743b47bac7e59aeb64717c
sidhant007 <sidhbansal@gmail.com>
2019-04-13 22:09:03 +0800

    Add destination input box. TODO: Make the heatmap function not take center as coordinates but instead take origin

--- static/main.js
+++ static/main.js
@@ -40,6 +40,9 @@ function initMap() {
   var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
+  var input2 = document.getElementById('pac2-input');
+  var searchBox2 = new google.maps.places.SearchBox(input2);
+
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
     searchBox.setBounds(map.getBounds());
@@ -56,10 +59,61 @@ function initMap() {
     }
 
     // Clear out the old markers.
+    /*
     markers.forEach(function(marker) {
       marker.setMap(null);
     });
     markers = [];
+    */
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+    map.setZoom(ZOOM_LEVEL);
+    update_heatmap();
+  });
+  searchBox2.addListener('places_changed', function() {
+    var places = searchBox2.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    /*
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+    */
 
     // For each place, get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
